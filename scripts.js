c7d752ccd324559089946b4e9848fb90fea32724
gusavocarnovale <gustavocarnovale@gmail.com>
2021-07-26 22:34:10 -0300

    Primer commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const db = firebase.database();
+
+window.onload = (event) =>{
+    db.ref('visits').once('value', (snapshot) => {
+        const info = Object.keys(snapshot.val()).length;
+        $("#muestraVisita").val(parseInt(info))
+    })
+
+    db.ref('visits').push(1);
+}
+
